00363fb6bb20fdee46d2425d667c3ac4a671ee44
Manivannan <manitinku38@gmail.com>
2024-03-06 16:59:26 +0530

    full functional update

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -14,11 +14,11 @@ const SideBar = ({selectedCategory, setSelectedCategory }) => (
     {categories.map((category) => (
       <button className="category-btn" 
         onClick={() => setSelectedCategory(category.name)}
-        style={{ background: category.name===selectedCategory && '#fc1503', color:'white' }}
+        style={{ background: category.name===selectedCategory && '#272727', color:'white' }}
         key={category.name}
       >
-        <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{category.icon}</span>
-        <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
+        <span style={{ color: category.name === selectedCategory ? 'white' : 'white', marginRight: '15px' }}>{category.icon}</span>
+        <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8', color: category.name === selectedCategory ? 'white' : 'white'}}>{category.name}</span>
       </button>
     ))}
   </Stack>
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,35 +4,38 @@ import { CheckCircle } from '@mui/icons-material'
 
 import { demoThumbnailUrl, demoVideoUrl, demoChannelUrl, demoChannelTitle, demoVideoTitle } from '../utils/constants'
 
-const VideoCard = ({ video: { id: {videoId }, snippet } }) => {
+const VideoCard = ({ video: { id: {videoId }, snippet }, position }) => {
     
   return (
-    <Card sx={{ width : { md: '320px', xs:'100%'}, boxShadow:'none', borderRadius:0 }}>
+    <Card sx={{ width : { xs:'100%',sm:'358px', md: '320px'}, boxShadow:'none', borderRadius:'8px', position:position }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl }>
             <CardMedia 
               image={snippet?.thumbnails?.high?.url}
               alt={snippet?.title}
-              sx = {{ width:358, height:180  }}
+              sx = {{ width:{
+                xs:'100%', sm:'358px', md: '320px'
+              }, height:160, }}
             />
-        </Link>
+        
 
-        <CardContent
-          sx={{backgroundColor:'#1e1e1e', height:'106px' }}>
+        <CardContent className='video-card'
+          sx={{backgroundColor:'black', height:'100px' }}>
             <Link to={videoId ? `/video/${videoId}` : demoVideoUrl }>
                 <Typography variant='subtitle1' color='#fff' fontWeight='bold'>
-                    {snippet?.title.slice(0,60) || snippet?.demoVideoTitle.slice(0,60)}
+                    {snippet?.title.slice(0,55) || snippet?.demoVideoTitle.slice(0,45)}
                 </Typography>
             </Link>
 
             <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl }>
-                <Typography variant='subtitle1' color='#fff' fontWeight='bold' display='flex' alignItems='center'>
-                    {snippet?.channelTitle.slice(0,60) || snippet?.demoChannelTitle.slice(0,60)}
+                <Typography className='channel-name' variant='subtitle1' color='#f1f1f1' display='flex' alignItems='center'>
+                    {snippet?.channelTitle.slice(0,45) || snippet?.demoChannelTitle.slice(0,45)}
                     <CheckCircle sx={{fontSize: '22px', color:'gray',ml: '5px'}} ></CheckCircle>
                 </Typography>
             </Link>
 
 
           </CardContent>
+          </Link>
     </Card>
   )
 }
