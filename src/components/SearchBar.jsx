3d5f07a369ebb4141c97a1aa333b42c026fde125
Manivannan <manitinku38@gmail.com>
2024-03-06 13:06:29 +0530

    searchBar updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Paper, IconButton } from '@mui/material'
-import { Search } from '@mui/icons-material'
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
+  };
+
   return (
     <Paper
-      component = 'form'
-      onSubmit={() => {}}
-      sx = {{
+      component='form'
+      onSubmit={onhandleSubmit}
+      sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
         pl: 2,
         boxShadow: 'none',
-        mr : { sm: 5 }
+        mr: { sm: 5 },
       }}
     >
-      <input type=""
-        placeholder='Search...'
+      <input
         className='search-bar'
-        value=''
-        onChange={() => {}} 
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-
-      <IconButton sx={{color:'red', p:'10px'}}>
-        <Search />
+      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+        <SearchIcon />
       </IconButton>
-
     </Paper>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,10 @@
+import React from 'react'
 import { Stack, Box } from '@mui/material'
 import VideoCard from './VideoCard'
 import ChannelCard from './ChannelCard'
 
 const Videos = ({ videos }) => {
-  
-  console.log(videos)
+
     return (
     <Stack
       direction='row' flexWrap='wrap' justifyContent='start' gap={2}
