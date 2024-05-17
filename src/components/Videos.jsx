a8e3aa62f19f7a6883572615002fcc617fac7065
tinku <96440547+manivannan-sudha@users.noreply.github.com>
2024-05-17 17:03:08 +0000

    v1.update.01

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import { Stack, Box } from '@mui/material'
-import VideoCard from './VideoCard'
-import ChannelCard from './ChannelCard'
-
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
+//
 const Videos = ({ videos, direction }) => {
-    if(!videos?.length) return 'Loading...'
-    return (
+  if (!videos?.length) return "Loading...";
+  return (
     <Stack
-      direction={direction || 'row'} flexWrap='wrap' justifyContent='start' gap={2}
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
     >
-        {videos.map((item, idx)=> (
-            <Box key={idx}>
-              {item.id.videoId && <VideoCard video={item} />}
-              {item.id.channelId && <ChannelCard channelDetail={item} />}
-            </Box>
-        ))}
-
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
     </Stack>
-  )
-}
+  );
+};
 
-export default Videos
+export default Videos;
